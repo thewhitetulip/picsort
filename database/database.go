c706a0f89e0203c47c3b59ab46fd2b539ed0d9b5
thewhitetulip <sapatil@live.com>
2015-11-11 10:13:26 +0530

    bug fixing

--- database/database.go
+++ database/database.go
@@ -1,42 +1,44 @@
 package database
 
 import (
-	"fmt"
-	_"github.com/mattn/go-sqlite3" 
 	"database/sql"
-	"strings"
-	"os"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"io/ioutil"
+	"os"
+	"strings"
 )
 
 var db *sql.DB
 var err error
+var tag string
+var name string
+var tags string
 
 func SortImages() {
 	distinctTags := "select distinct tags from picture;"
-	var tag string
 	result := "./public/result"
 	tagRows, errRows := db.Query(distinctTags)
 	checkError(errRows, "sortImages")
 	os.Chdir(result)
-	for tagRows.Next(){
+	for tagRows.Next() {
 		errRows = tagRows.Scan(&tag)
 		itag := strings.Split(tag, ",")
-		
+
 		for _, value := range itag {
 			tag = strings.Trim(value, " ")
 			fileerr := os.Mkdir(tag, 0777)
 			checkError(fileerr, "sortImages")
 		}
 	}
-	
+
+	tagRows.Close()
+
 	sqlQuery := "select name, tags from picture where length(tags)>0;"
 	os.Chdir("../")
-	var name string
-	var tags string
 	rows, err := db.Query(sqlQuery)
 	checkError(err, "sortImages")
-	for rows.Next(){
+	for rows.Next() {
 		err = rows.Scan(&name, &tags)
 		all_tags := strings.Split(tags, ",")
 		for _, value := range all_tags {
@@ -48,11 +50,12 @@ func SortImages() {
 			checkError(err, "file copy")
 		}
 	}
+	rows.Close()
 }
 
 func checkError(err error, name string) {
 	if err != nil {
-		fmt.Println(err , " ",  name)
+		fmt.Println(err, " ", name)
 	}
 }
 func Initialize() {
@@ -60,10 +63,10 @@ func Initialize() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	
-	rows, err := db.Query("select count(*) from picture");
+
+	rows, err := db.Query("select count(*) from picture")
 	checkError(err, "select count")
-	
+
 	var count int
 	insertStmt := "insert into picture(name) values("
 	if rows.Next() {
@@ -72,47 +75,62 @@ func Initialize() {
 	fmt.Println(count)
 	rows.Close()
 	if count == 0 {
-			files, err := ioutil.ReadDir("./public")
-			checkError(err, "reading files")
-			for _, value := range files {
-				if !value.IsDir() {
-					_, err := db.Exec(insertStmt+"\""+strings.Trim(value.Name(), " ")+"\");")
-					checkError(err, "inserting values")
-				}
+		files, err := ioutil.ReadDir("./public")
+		checkError(err, "reading files")
+		for _, value := range files {
+			if !value.IsDir() {
+				_, err := db.Exec(insertStmt + "\"" + strings.Trim(value.Name(), " ") + "\");")
+				checkError(err, "inserting values")
 			}
 		}
+	}
 }
 
-func GetPhoto()  (string, int) {	
+func GetPhoto() (string, int) {
 	var name string
 	var remaining int
-	
+
 	rows, err := db.Query("select name from picture where tags is null limit 1;")
-	defer rows.Close()
 	checkError(err, "GetPhoto")
-	
+
 	if rows.Next() {
 		err = rows.Scan(&name)
 		checkError(err, "GetPhoto")
 	}
-	
-	defer rows.Close()
+
+	rows.Close()
+
 	rows, err = db.Query("select count(*) from picture where tags is null limit 1;")
 	checkError(err, "GetPhoto")
-	
-	if rows.Next(){
+
+	if rows.Next() {
 		err = rows.Scan(&remaining)
-		checkError(err, "GetPhoto")		
+		checkError(err, "GetPhoto")
 	}
+
+	rows.Close()
 
 	return name, remaining
 }
 
-
 func Update(tags string, photoname string) bool {
 	var updateQuery string = "update picture set tags = ? where name = ?"
-	_, err := db.Exec(updateQuery, tags, photoname)
-	if err!=nil{
+	_, err = db.Exec(updateQuery, tags, photoname)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	return true
+}
+
+func Close() {
+	db.Close()
+}
+
+func DeleteImages() bool {
+	var deleteQuery string = "delete from picture"
+	_, err = db.Exec(deleteQuery)
+	if err != nil {
 		fmt.Println(err)
 		return false
 	}
--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
+
 import (
-	"net/http"
 	"fmt"
-	"text/template"
 	"github.com/thewhitetulip/picsort/database"
+	"net/http"
+	"text/template"
 )
 
 type Photo struct {
-    Name string
-    Remaining int
+	Name      string
+	Remaining int
 }
+
+var t *template.Template
+var tmpl *template.Template
+var err error
 
 func baseHandler(w http.ResponseWriter, r *http.Request) {
 	method := r.Method
@@ -19,28 +24,46 @@ func baseHandler(w http.ResponseWriter, r *http.Request) {
 		photoname := r.Form.Get("photoname")
 		database.Update(tags, photoname)
 	}
-	t, _ := template.ParseFiles("home.gtpl")
+
 	photo_name, remaining := database.GetPhoto()
 	p := &Photo{
-        Name: photo_name,
-        Remaining: remaining,
+		Name:      photo_name,
+		Remaining: remaining,
 	}
-	
+
 	t.Execute(w, p)
 }
 
-
-func imageHandler(w http.ResponseWriter, r *http.Request){
+func imageHandler(w http.ResponseWriter, r *http.Request) {
 	database.SortImages()
-	w.Write([]byte("Sorting done"))
+	tmpl.Execute(w, nil)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	truth := database.DeleteImages()
+	if truth != false {
+		w.Write([]byte("Delete done. stop the server now, output is copied in public/result folder! please cut the folders after you are done"))
+	}
 }
 
 func main() {
+	t, err = template.ParseFiles("home.gtpl")
+	if err != nil {
+		fmt.Println("Error parsing file")
+	}
+	message := "<html>sorting done, <a href='/delete'> delete </a> db entries and old files?</html>"
+	tmpl, err = template.New("delete").Parse(message)
+
+	if err != nil {
+		fmt.Println("Error parsing file")
+	}
+
 	PORT := ":8080"
 	database.Initialize()
-	
+	defer database.Close()
 	http.HandleFunc("/", baseHandler)
 	http.HandleFunc("/sort/", imageHandler)
+	http.HandleFunc("/delete/", deleteHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public/"))))
 	fmt.Println("running on port 8080\n")
 	fmt.Println(http.ListenAndServe(PORT, nil))
